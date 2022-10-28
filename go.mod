31dc06521c154ffda716178f637f1b1ac6e7445d
veremchukvv@gmail.com <veremchukvv@gmail.com>
2022-10-28 21:54:52 +0300

    ver1

---
+++ go.mod
@@ -0,0 +1,5 @@
+module scheduler
+
+go 1.18
+
+require github.com/golang/glog v1.0.0
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"flag"
+	"sync"
+	"time"
+
+	"github.com/golang/glog"
+)
+
+type Sched struct {
+	Fn      func()
+	LastRun bool
+	mu      sync.Mutex
+}
+
+func NewSched(fn func()) *Sched {
+	return &Sched{Fn: fn}
+}
+
+func main() {
+	flag.Parse()
+
+	// ctx := context.Background()
+	sched := NewSched(fn)
+	sched.Start()
+	// sched.Stop()
+}
+
+func fn() {
+	time.Sleep(time.Millisecond * 1000) // Имитация 1 секунды длительности работы
+	glog.Info(">>fn")                   // Вывод в лог факта завершения выполнения
+}
+
+func (s *Sched) Start() {
+	var wg sync.Once
+
+	ch := time.After(time.Millisecond * 1530)
+
+LOOP:
+	for {
+		select {
+		case <-ch:
+			glog.Info(">>exit")
+			if s.LastRun {
+				wg.Do(fn)
+			}
+			break LOOP
+		default:
+			if s.mu.TryLock() {
+				go func() {
+					fn()
+					s.mu.Unlock()
+				}() // Вызов функции в отдельном потоке
+			} else {
+				s.LastRun = true
+			}
+		}
+	}
+}
+
+// func (s *Sched) Stop() {
+// 	var wg sync.Once
+
+// 	if s.LastRun {
+// 		wg.Do(fn)
+// 	}
+// }
